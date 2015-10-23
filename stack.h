13878cf02386ed9b14d0fd75c6315e6a79b7eba2
Kirai <2014011234@mail.bistu.edu.cn>
2015-10-23 18:01:18 +0800

    Update stack.h

--- stack.h
+++ stack.h
@@ -13,7 +13,7 @@ typedef List Stack;
 #define stack_init list_init
 #define stack_destory list_destory;
 int stack_push(Stack *stack, const void *data);
-int stack_pop(Stock *stack, void **data);
+int stack_pop(Stack *stack, void **data);
 #define stack_peek(stack)((stack)->head == NULL ? NULL : (stack)->head->data)
 #define stack_size list_size
 #endif
